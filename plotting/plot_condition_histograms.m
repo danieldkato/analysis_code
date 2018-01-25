ee9b357f699165b17e4b685fffdab4393388756c
Dan Kato <daniel.d.kato@gmail.com>
2018-01-25 16:56:58 -0500

    Update documentation for plot_condition_histograms.m

--- plotting/plot_condition_histograms.m
+++ plotting/plot_condition_histograms.m
@@ -1,5 +1,43 @@
 function hist_figure = plot_condition_histograms(Conditions, fig_title)
+% DOCUMENTATION TABLE OF CONTENTS:
 
+% I. OVERVIEW
+% II. REQUIREMENTS
+% III. INPUTS
+% IV. OUTPUTS
+
+% Last updated DDK 2018-01-25 
+
+
+%% I. OVERVIEW: 
+% This function plots histograms of some distribution associated with each
+% trial/stimulus condition within a single imaging session. 
+
+
+%% II. REQUIREMENTS:
+% 1) MATLAB v >= ???
+
+
+%% III. INPUTS: 
+% 1) Conditions - c x 1 array of structs, where c is the number of
+%    conditions in which the current neuron was observed. Each element
+%    of Conditions should minimally include the following fields:
+%           
+%       Distribution - 1 x d vector specifying some distributuon associated
+%       with the corresponding condition, where d is the number of
+%       observations in the distribution.
+%
+%       Name - char array specifying human-readable condition name 
+%
+%       Color - 1 x 3 RGB vector specifying the color code for the
+%       corresponding condition.
+
+
+%% IV. OUTPUTS:
+% 1) mean_figure - handle to created figure
+
+
+%% Create figure:
 hist_figure = figure();
 title(fig_title);
 num_conditions = length(Conditions);
