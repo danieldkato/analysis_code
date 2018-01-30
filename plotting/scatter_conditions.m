75315da283cb5e8a2554a0d187321b35f906bc96
Dan Kato <daniel.d.kato@gmail.com>
2018-01-29 21:34:50 -0500

    Make scatter_conditions.m take a fig_title argument

--- plotting/scatter_conditions.m
+++ plotting/scatter_conditions.m
@@ -1,4 +1,4 @@
-function h = scatter_conditions(y_cond_name, x_cond_name, Conditions, mouse, session)
+function h = scatter_conditions(y_cond_name, x_cond_name, Conditions, fig_title)
 
 % Get requested condition info:
 X = get_condition(x_cond_name, Conditions);
@@ -46,10 +46,6 @@ if defines_color
     Y.label.Color = Y.color;
 end
 
-%Add title
-base_title = ['Peak dF/F responses to \color[rgb]{' num2str(Y.color) '} ' Y.abbreviation '\color[rgb]{0 0 0} vs \color[rgb]{' num2str(X.color) '}' X.abbreviation '\color[rgb]{0 0 0}'];
-fig_title = base_title;
-
 if nargin > 3
     fig_title = {base_title; ['Mouse ' mouse]};
 end
@@ -60,9 +56,7 @@ end
 
 title(fig_title, 'FontWeight', 'normal');
 
-%{
 % Add title: 
 if nargin > 3
     title(fig_title, 'FontWeight', 'normal');
 end
-%}
