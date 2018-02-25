7ba1e10a7169e085f706a8c57e93692279bcc9a6
Dan Kato <daniel.d.kato@gmail.com>
2018-02-25 18:30:04 -0500

    Add get_condition_AUC.m

---
+++ format_data/get_condition_AUC.m
@@ -0,0 +1,14 @@
+function Conditions = get_condition_AUC(Conditions, start, stop)
+
+for c = 1:length(Conditions)
+    
+    mean_traces = Conditions(c).Mean;
+    
+    if nargin < 2
+        abs_peaks = sum(mean_traces(:, start:stop), 2);
+    else
+        abs_peaks = max(mean_traces(:, start:end), 2);
+    end
+    
+    Conditions(c).AUC = abs_peaks;
+end
