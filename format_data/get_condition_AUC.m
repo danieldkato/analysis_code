2d390a4b3ef153eb5895d45ed870669347de75ab
Dan Kato <daniel.d.kato@gmail.com>
2018-02-25 19:38:45 -0500

    Give variables in get_condition_AUC more appropriate names

--- format_data/get_condition_AUC.m
+++ format_data/get_condition_AUC.m
@@ -5,10 +5,10 @@ for c = 1:length(Conditions)
     mean_traces = Conditions(c).Mean;
     
     if nargin < 2
-        abs_peaks = sum(mean_traces(:, start:stop), 2);
+        AUC = sum(mean_traces(:, start:stop), 2);
     else
-        abs_peaks = sum(mean_traces(:, start:end), 2);
+        AUC = sum(mean_traces(:, start:end), 2);
     end
     
-    Conditions(c).AUC = abs_peaks;
+    Conditions(c).AUC = AUC;
 end
